a1fda5a134c57d209ac3ee1a01451178c7f57d97
assertive-git <a.ruiz@assertive.co.jp>
2024-04-03 17:20:33 +0900

    home route

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function index()
+	public function index($page = 'home')
 	{
-		$this->load->view('home');
+		$this->load->view($page);
 	}
 }
