813f101e80ac25eac079ab2196250f0120eb4641
assertive-git <a.ruiz@assertive.co.jp>
2024-04-03 17:02:19 +0900

    sessions folder

--- application/config/config.php
+++ application/config/config.php
@@ -388,6 +388,11 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
+
+if(file_exists(APPPATH . 'sessions')) {
+    mkdir(APPPATH . 'sessions', 0757);
+}
+
 $config['sess_save_path'] = APPPATH . 'sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
