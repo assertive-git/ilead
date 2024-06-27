9ef60d427190879285d2858f4d8ffce5cb8b36fc
hayashi <m.hayashi@assertive.co.jp>
2024-06-27 19:03:32 +0900

    上書きしました

--- application/views/news_list.php
+++ application/views/news_list.php
@@ -1,36 +1,1 @@
-<?php include ('header.php'); ?>
-
-<main id="news_list">
-
-  <section class="news">
-    <div class="news_inner">
-      <div class="text">
-        <h2><span>お知らせ</span> News</h2>
-      </div>
-      <ul class="news_area">
-        <?php foreach ($news as $article): ?>
-          <a href="/news/<?= $article['id'] ?>">
-            <li><span class="day"><?= substr(str_replace('-', '.', $article['created_at']), 0, 11) ?></span><span
-                class="title"><?= $article['title'] ?></span></li>
-          </a>
-        <?php endforeach ?>
-      </ul>
-    </div>
-
-    <div class="pagination">
-      <button class="arrow_before"></button>
-      <div class="page">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-      </div>
-      <button class="arrow_next"></button>
-    </div>
-  </section>
-
-
-</main>
-
-
-<?php include ('footer.php'); ?>
+<?php include ('header.php'); ?><main id="news_list">  <section class="news">    <div class="news_inner">      <div class="text">        <h2><span>お知らせ</span> News</h2>      </div>      <ul class="news_area">        <?php foreach ($news as $article): ?>          <a href="/news/<?= $article['id'] ?>">            <li><span class="day"><?= substr(str_replace('-', '.', $article['created_at']), 0, 11) ?></span><span                class="title"><?= $article['title'] ?></span></li>          </a>        <?php endforeach ?>      </ul>    </div>        <div class="pagination">      <button class="arrow_before"></button>      <div class="page">        <button>1</button>        <button>2</button>        <button>3</button>        <button>4</button>      </div>      <button class="arrow_next"></button>    </div>  </section></main><?php include ('footer.php'); ?>
