56bd4c0915c4f77746d03306bfb4e9e3e8d9693c
Adrian <seraphzp@gmail.com>
2025-01-21 10:20:18 +0900

    メールアドレス変更

--- application/views/sendmail.php
+++ application/views/sendmail.php
@@ -27,7 +27,7 @@ mb_internal_encoding("UTF-8");
 // ----- メール送信
 //$admin_mail = "s.sugiyama@assertive.co.jp";
 $from = "contact@ilead.trend-search.info";
-$admin_mail = "h.nakayama@assertive.co.jp,a.ruiz@assertive.co.jp";
+$admin_mail = "h.nakayama@assertive.co.jp";
 
 
 //$body = "";
