2cc9930fdd6aa29c29dbbc8828e00a0e7a7b3a23
a.ruiz <a.ruiz@assertive.co.jp>
2025-03-10 16:07:59 +0900

    updatE

--- application/views/complete.php
+++ application/views/complete.php
@@ -1,1 +1,20 @@
-<?php include ('header.php'); ?><div id="form" class="complete">  <div class="contact_h">送信完了</div>        <div class="progressbar">      <div class="item">STEP.1 ご入力</div>      <div class="item">STEP.2 ご確認</div>      <div class="item active">STEP.3 完了</div>    </div>  <p> この度はご登録いただき、<br class="sp">    誠にありがとうございます。<br>    <br>    ご入力いただきましたメールアドレス宛に、<br>    登録受付完了メールを<br class="sp">    お送りさせていただいております。<br>    <br>    ご確認の程、よろしくお願い致します。<br>  </p>  <p class="top_button"><a href="././">トップへ戻る</a></p></div><?php include ('footer.php'); ?>
+<?php include ('header.php'); ?>
+<div id="form" class="complete">
+  <div class="contact_h">送信完了</div>
+        <div class="progressbar">
+      <div class="item">STEP.1 ご入力</div>
+      <div class="item">STEP.2 ご確認</div>
+      <div class="item active">STEP.3 完了</div>
+    </div>
+  <p> この度はご登録いただき、<br class="sp">
+    誠にありがとうございます。<br>
+    <br>
+    ご入力いただきましたメールアドレス宛に、<br>
+    登録受付完了メールを<br class="sp">
+    お送りさせていただいております。<br>
+    <br>
+    ご確認の程、よろしくお願い致します。<br>
+  </p>
+  <p class="top_button"><a href="/">トップへ戻る</a></p>
+</div>
+<?php include ('footer.php'); ?>
